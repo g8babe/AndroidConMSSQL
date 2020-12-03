38f266bc555c26082a17dde625aaa5db4e7f7d26
Jasmine <jasmine231031@gmail.com>
2020-12-03 19:02:57 +0800

    initial

--- app/src/androidTest/java/com/fju/mymssqlserverdatabaseconnection/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fju/mymssqlserverdatabaseconnection/ExampleInstrumentedTest.java
@@ -23,5 +23,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.fju.mymssqlserverdatabaseconnection", appContext.getPackageName());
+
     }
 }
